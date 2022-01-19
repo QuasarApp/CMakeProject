1c06bf8477a0ae70c340ba3a158ba9593f622cfa
EndrII <EndrIIMail@gmail.com>
2022-01-19 11:05:03 +0300

    major template update * fix copyright * fix library structure of the source codes * added support version value of the library * fix source location of the generated gocumentatio (powered by doxygen)

--- src/Library/src/public/RENAME_ME.cpp
+++ src/Library/src/public/RENAME_ME.cpp
@@ -1,6 +1,6 @@
 //#
-//# Copyright (C) 2021-2021 QuasarApp.
-//# Distributed under the lgplv3 software license, see the accompanying
+//# Copyright (C) 2021-2022 QuasarApp.
+//# Distributed under the GPLv3 software license, see the accompanying
 //# Everyone is permitted to copy and distribute verbatim copies
 //# of this license document, but changing it is not allowed.
 //#
@@ -13,6 +13,10 @@ namespace RENAME_ME {
 bool init() {
     initRENAME_MEResources();
     return true;
+}
+
+QString version() {
+    return RENAME_ME_VERSION;
 }
 
 
---
+++ src/Library/src/public/RENAME_ME.h
@@ -0,0 +1,27 @@
+//#
+//# Copyright (C) 2021-2022 QuasarApp.
+//# Distributed under the GPLv3 software license, see the accompanying
+//# Everyone is permitted to copy and distribute verbatim copies
+//# of this license document, but changing it is not allowed.
+//#
+
+#include "RENAME_ME/global.h"
+#include <QString>
+
+inline void initRENAME_MEResources() { Q_INIT_RESOURCE(RENAME_ME); }
+
+namespace RENAME_ME {
+
+/**
+ * @brief init main initialize method of The RENAME_ME library
+ * @return true if library initialized successfull
+ */
+bool RENAME_ME_EXPORT init();
+
+/**
+ * @brief version This method return string value of a library version
+ * @return string value of a library version
+ */
+QString RENAME_ME_EXPORT version();
+
+};
